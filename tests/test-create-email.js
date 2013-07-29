4e00f9bbafe7f2628a467a0cabfeec4467849f1a
snoj <josh@snoj.us>
2013-07-29 14:31:26 -0500

    add: test creation of message. fix: emailobj and msgpart getHeader() clean: moved header and header value regex to variables.

--- tests/test-create-email.js
+++ tests/test-create-email.js
@@ -1,15 +1,8 @@
-# email-obj
-
-An API for creating and parsing emails.
-
-# Use
-```
-var emailobj = require('email-obj');
-
+var assert = require('assert');
+var emailobj = require('../');
 var msg = new emailobj();
 msg.setHeader('subject', 'Hello!');
 msg.setHeader('from', 'yourmom@example.com');
 msg.setHeader('to', 'you@example.com');
 msg.data.push('Please remember that we going to be in town this weekend to visit. So be sure to have the spare room clean! We Love you!\r\n\r\n-Mom');
-msg.compile();
-```
+assert.equal(msg.getHeader('subject'), 'Hello!');
