b9014c4395e5a2130d2957a376f4778aa3db8e91
snoj <josh@snoj.us>
2013-07-28 23:08:34 -0500

    add: msg-part.js to handle multipart messages mod: some areas that needed tweaking

--- tests/test-email-obj.js
+++ tests/test-email-obj.js
@@ -3,3 +3,13 @@ var eo = require('../index.js');
 
 var m = eo.parse(require('fs').readFileSync('./tests/data/folding.txt').toString('ascii'));
 assert.equal(m.headers.subject, "test with whitespace! anmd more!");
+
+m.setHeader('subject', "another subject", true);
+assert.equal(m.headers.subject, "another subject");
+
+m.setHeader('content-type', "something/something");
+assert.equal(m.headers['content-type'], "something/something");
+
+m.setHeader('content-type', "somethingborrowed/something", true);
+assert.equal(m.headers['content-type'], "somethingborrowed/something");
+console.log(m.data);
